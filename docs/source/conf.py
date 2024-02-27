ccf8da3eb62473ab95f14a5b6f74b55dbcdae4df
Andre Hochschulz <andre.hochschulz@akkodis.com>
2024-02-27 14:50:08 +0100

    readthedocs init

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,10 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'amad'
-copyright = '2024, AKKODIS'
-author = 'Andre Hochschulz'
-release = '0.2.0'
+project = "amad"
+copyright = "2024, AKKODIS"
+author = "Andre Hochschulz"
+release = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -21,13 +21,14 @@ extensions = [
     "nbsphinx",
     "nbsphinx_link",
     "sphinx.ext.autodoc",
-    'sphinx.ext.autosummary',
-    'autoapi.extension'
+    "sphinx.ext.autosummary",
+    "autoapi.extension",
+    "sphinx_search.extension"
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-autoapi_dirs = ['../..']
+autoapi_dirs = ["../.."]
 autoapi_ignore = ["*/conf.py", "*/test_*.py"]
 
 # -- Options for HTML output -------------------------------------------------
@@ -35,5 +36,5 @@ autoapi_ignore = ["*/conf.py", "*/test_*.py"]
 
 # html_theme = 'alabaster', 'furo'
 # html_theme = 'furo'
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
